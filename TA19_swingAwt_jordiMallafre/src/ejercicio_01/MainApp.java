24bca3838d02039203fa22b63f1232bbc98a49d1
jordimall <jordi.mallafre@hotmail.com>
2023-10-25 18:13:51 +0200

    Finalicación primer ejercició

---
+++ TA19_swingAwt_jordiMallafre/src/ejercicio_01/MainApp.java
@@ -0,0 +1,12 @@
+package ejercicio_01;
+
+import views.ViewEjercicio01;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ViewEjercicio01 frame = new ViewEjercicio01();
+
+	}
+
+}
